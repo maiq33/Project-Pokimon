ede4d6ce201620ac78cdf151c8d6a3adde0d5611
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-03-07 03:26:47 +0000

    vamos bien

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const canvas  = document.querySelector('canvas')
 
-console.log(canvas)
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 512
+
+
+console.log(c)
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = '/workspaces/Project-Pokimon/Chris Courses - Pokemon/Images/Pellet Town.png'
+console.log(image)
+
+//c.drawImage('/workspaces/Project-Pokimon/Chris Courses - Pokemon/Images/Pellet Town.png')
