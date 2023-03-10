8a9e29c47f771f8cfdfb0375fb7b31c40d5050df
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-03-10 03:30:17 +0000

    1:38 Miguelon, que pro

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Project-Pokimon
-Proyecto de Chris courses min 1:20:00 - https://www.youtube.com/watch?v=yP5DKzriqXA&amp;list=PLpPnRKq7eNW16Wq1GQjQjpTo_E0taH0La&amp;index=5
+Proyecto de Chris courses min 1:38:00 - https://www.youtube.com/watch?v=yP5DKzriqXA&amp;list=PLpPnRKq7eNW16Wq1GQjQjpTo_E0taH0La&amp;index=5
--- index.js
+++ index.js
@@ -13,12 +13,26 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = 'ChrisCourses_Pokemon/Images/Pellet_Town_Big.png'
-console.log(image)
+
+const playerImage = new Image()
+playerImage.src = 'ChrisCourses_Pokemon/Images/playerDown.png'
 
 image.onload = () => {
-    c.drawImage(image, -700, -500)
-}
+    c.drawImage(image, -530, -580);
+  }
 
+playerImage.onload = () => {
+    c.drawImage(playerImage, 
+        canvas.width / 2 - playerImage.width / 2, 
+        canvas.height / 2 - playerImage.height / 2);
+  }
+
+
+// Manejador de eventos para el caso de que una imagen no se cargue correctamente
 image.onerror = () => {
     console.error("Error al cargar la imagen");
   };
+
+console.log(playerImage)
+console.log(playerImage.x)
+console.log(playerImage.y)
